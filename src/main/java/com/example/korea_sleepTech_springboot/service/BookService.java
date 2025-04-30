b19f59e1f6c56b33d32393b12a4fe9b128780ef6
mijinn0 <pmj_0818@naver.com>
2025-04-30 14:15:49 +0900

    20250430 springboot book (ResponseEntity & ResponseDto)

--- src/main/java/com/example/korea_sleepTech_springboot/service/BookService.java
+++ src/main/java/com/example/korea_sleepTech_springboot/service/BookService.java
@@ -3,7 +3,9 @@ package com.example.korea_sleepTech_springboot.service;
 import com.example.korea_sleepTech_springboot.dto.request.BookCreateRequestDto;
 import com.example.korea_sleepTech_springboot.dto.request.BookUpdateRequestDto;
 import com.example.korea_sleepTech_springboot.dto.response.BookResponseDto;
+import com.example.korea_sleepTech_springboot.dto.response.ResponseDto;
 import com.example.korea_sleepTech_springboot.entity.C_Book;
+import com.example.korea_sleepTech_springboot.entity.C_Category;
 import com.example.korea_sleepTech_springboot.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,8 +18,7 @@ import java.util.stream.Collectors;
 public class BookService {
     private final BookRepository bookRepository;
 
-    public BookResponseDto createBook(BookCreateRequestDto requestDto) {
-        BookResponseDto responseDto = null;
+    public ResponseDto<BookResponseDto> createBook(BookCreateRequestDto requestDto) {
         try {
             C_Book newBook = new C_Book(
                     null, requestDto.getWriter(), requestDto.getTitle(), requestDto.getContent(), requestDto.getCategory()
@@ -25,17 +26,18 @@ public class BookService {
 
             C_Book savedBook = bookRepository.save(newBook);
 
-            responseDto = new BookResponseDto(
+            BookResponseDto response = new BookResponseDto(
                     savedBook.getWriter(),
                     savedBook.getTitle(),
                     savedBook.getCategory()
             );
 
+            return ResponseDto.setSuccess("새로운 책이 정상적으로 등록되었습니다.", response);
+
         } catch (Exception e) {
             e.printStackTrace();
+            return ResponseDto.setFailed("게시글 등록 중 오류가 발생하였습니다: " + e.getMessage());
         }
-
-        return responseDto;
     }
 
     public List<BookResponseDto> getAllBooks() {
@@ -113,6 +115,78 @@ public class BookService {
 
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public ResponseDto<List<BookResponseDto>> getBookByTitleContaining(String keyword) {
+        List<BookResponseDto> books = null;
+
+        try {
+            List<C_Book> booksByTitleContaining = bookRepository.findByTitleContaining(keyword);
+
+            books = booksByTitleContaining.stream()
+                    .map(book -> new BookResponseDto(
+                            // 작성자, 제목, 카테고리
+                            book.getWriter(),
+                            book.getTitle(),
+                            book.getCategory()
+                    ))
+                    .collect(Collectors.toList());
+
+            return ResponseDto.setSuccess("책 검색을 정상적으로 완료하였습니다.", books);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseDto.setFailed("책을 검색하는 동안 문제가 발생하였습니다.");
+        }
+    }
+
+    public ResponseDto<List<BookResponseDto>> getBookByCategory(C_Category category) {
+        List<BookResponseDto> books = null;
+
+        try {
+            List<C_Book> booksByCategory = bookRepository.findByCategory(category);
+
+            books = booksByCategory.stream()
+                    . map(book -> new BookResponseDto(
+                            book.getWriter(),
+                            book.getTitle(),
+                            book.getCategory()
+                    ))
+                    .collect(Collectors.toList());
+
+            return ResponseDto.setSuccess("카테고리별 책 조회가 성공하였습니다.", books);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseDto.setFailed("카테고리별 책을 검색하는 동안 문제가 발생하였습니다.");
+        }
+    }
+
+    public ResponseDto<List<BookResponseDto>> getBookByCategoryAndWriter(C_Category category, String writer) {
+        List<BookResponseDto> books = null;
+
+        List<C_Book> searchingBook = null;
+        try {
+            if (category == null) {
+                searchingBook = bookRepository.findByWriter(writer);
+            } else {
+                searchingBook = bookRepository.findByCategoryAndWriter(category, writer);
+            }
+
+            books = searchingBook.stream()
+                    .map(book -> new BookResponseDto(
+                            book.getWriter(),
+                            book.getTitle(),
+                            book.getCategory()
+                    ))
+                    .collect(Collectors.toList());
+
+            return ResponseDto.setSuccess("조회가 완료되었습니다.", books);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseDto.setFailed("카테고리와 작성자로 조회하는 동안 문제가 발생하였습니다.");
         }
     }
 }
